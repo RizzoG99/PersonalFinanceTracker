cff633de3fb8443afc24ae74bef7c560392ddd6a
RizzoG99 <gabriele.rizzo.ph@gmail.com>
2026-08-08 13:59:59 +0200

    chore: bootstrap App Group entitlement and SafeToSpendWidget extension target

---
+++ PersonalFinanceTraker/SafeToSpendWidget/SafeToSpendWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SafeToSpendWidgetBundle.swift
+//  SafeToSpendWidget
+//
+//  Created by Gabriele Rizzo on 08/08/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SafeToSpendWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SafeToSpendWidget()
+        SafeToSpendWidgetControl()
+    }
+}
